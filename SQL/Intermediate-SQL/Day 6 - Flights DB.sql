32e28aabb88892147f139fec81a5abdf7198a069
Bangga Adi <banggaadi@gmail.com>
2024-03-25 23:24:26 +0700

    Day 6

--- SQL/Intermediate-SQL/Day 6 - Flights DB.sql
+++ SQL/Intermediate-SQL/Day 6 - Flights DB.sql
@@ -40,6 +40,7 @@ FROM seats s
 LEFT JOIN boarding_passes b
 ON s.seat_no = b.seat_no
 WHERE b.boarding_no IS NULL
+
 -- FIND WHICH SEAT LINE IS FAVORITE
 SELECT RIGHT(s.seat_no, 1) AS seat_line, COUNT(*)
 FROM seats s
@@ -47,3 +48,36 @@ LEFT JOIN boarding_passes b
 ON s.seat_no = b.seat_no
 GROUP BY seat_line
 ORDER BY COUNT(*) DESC
+
+-- AVG PRICE FOR DIFFERENT SEAT NUMBER
+SELECT *
+FROM boarding_passes
+
+SELECT *
+FROM ticket_flights
+
+SELECT seat_no, ROUND(AVG(amount),2) 
+FROM ticket_flights tf
+LEFT JOIN boarding_passes bp
+ON tf.ticket_no = bp.ticket_no
+AND tf.flight_id = bp.flight_id
+GROUP BY seat_no
+ORDER BY 2 DESC
+
+-- CHALLENGE
+SELECT passenger_name,SUM(total_amount) FROM tickets t
+INNER JOIN bookings b
+ON t.book_ref=b.book_ref
+GROUP BY passenger_name
+ORDER BY SUM(total_amount) DESC
+
+SELECT passenger_name, fare_conditions, COUNT(fare_conditions) as use
+FROM tickets ti
+LEFT JOIN ticket_flights tf
+ON ti.ticket_no = tf.ticket_no
+WHERE passenger_name = 'ALEKSANDR IVANOV'
+GROUP BY passenger_name, fare_conditions
+ORDER BY use DESC
+
+
+
